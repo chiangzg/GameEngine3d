cf471c79603a37c14970172a43763b1be695c0d8
Chiang <chiangzg@qq.com>
2018-06-01 00:36:46 +0800

    init dir

---
+++ Framework/Interface/IApplication.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Interface.hpp"
+#include "IRuntimeModule.hpp"
+
+namespace Engine {
+	Interface IApplication: implements IRuntimeModule
+	{
+	public:
+		virtual int Initialize() = 0;
+		virtual void Finalize() = 0;
+		virtual void Tick() = 0;
+	
+		//处理退出信号	
+		virtual bool IsQuit() = 0;
+	}
+}
