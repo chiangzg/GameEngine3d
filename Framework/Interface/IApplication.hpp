9d333ed221089807c431ac7c89966a9c63f848a7
chiangzg <chiangzg@gmail.com>
2018-12-05 13:43:33 +0800

    格式化代码

--- Framework/Interface/IApplication.hpp
+++ Framework/Interface/IApplication.hpp
@@ -4,14 +4,15 @@
 #include "IRuntimeModule.hpp"
 
 namespace Engine {
-	Interface IApplication: implements IRuntimeModule
-	{
-	public:
-		virtual int Initialize() = 0;
-		virtual void Finalize() = 0;
-		virtual void Tick() = 0;
-	
-		//处理退出信号	
-		virtual bool IsQuit() = 0;
-	}
+    Interface IApplication : implements IRuntimeModule {
+    public:
+        virtual int Initialize() = 0;
+
+        virtual void Finalize() = 0;
+
+        virtual void Tick() = 0;
+
+        //处理退出信号
+        virtual bool IsQuit() = 0;
+    }
 }
--- Framework/Interface/IRuntimeModule.hpp
+++ Framework/Interface/IRuntimeModule.hpp
@@ -3,14 +3,17 @@
 #include "Interface.hpp"
 
 namespace Engine {
-	Interface IRuntimeModule {
-		public:
-			virtual ~IRuntimeModule() {};
-			//初始化模块
-			virtual int Initialize() = 0;
-			//最终垃圾回收
-			virtual void Finalize() = 0;
-			//驱动执行，每执行一次处理一个单位
-			virtual void Tick() = 0;
-	};
+    Interface IRuntimeModule {
+    public:
+        virtual ~IRuntimeModule() {};
+
+        //初始化模块
+        virtual int Initialize() = 0;
+
+        //最终垃圾回收
+        virtual void Finalize() = 0;
+
+        //驱动执行，每执行一次处理一个单位
+        virtual void Tick() = 0;
+    };
 }
